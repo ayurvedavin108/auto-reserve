fc38293c4bdc0d373e37ecf427f4273f8e1a6e52
ayurvedavin108 <ayurvedavin108@gmail.com>
2026-03-23 15:56:40 +0200

    Update reserve.py

--- reserve.py
+++ reserve.py
@@ -104,7 +104,7 @@ def reserve():
     #(//div[@class='name'])[2]
     #//*[@id="warehouseSelectComponent"]/div/div/div/div/div[2]/div[2]
     #//label[text()='Склад']/following-sibling::div[@class='name'] резервный XPATH
-    warehouse = (By.XPATH, '(//div[@class='name'])[2]'))
+    warehouse = (By.XPATH, "(//div[@class='name'])[2]")
     wait.until(EC.presence_of_element_located(warehouse))
     warehouse_trigger = wait.until(EC.element_to_be_clickable(warehouse))
     time.sleep(2)
